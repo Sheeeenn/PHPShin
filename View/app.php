f60cb936b1f7bb4c4e0a5b10b2b548ea14b26452
guiribajustin2004 <127721330+guiribajustin2004@users.noreply.github.com>
2024-09-29 20:02:04 +0800

    WebPage

--- View/app.php
+++ View/app.php
@@ -1,1 +1,27 @@
-<h1>HII2</h1>
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+    
+        <title>PHPShin</title>
+
+        <link rel="icon" type="image/icon" href="PHPShin/images/rounded_logo.png">
+
+        <link rel="stylesheet" type="text/css" href="View/css/app.css">
+        
+    </head>
+
+    <body>
+
+        <nav>
+            
+            <img src="PHPShin/images/text_logo.png" alt="Shin-Logo" class="logo-img">
+            
+            <h4 class="version-text" >Shin v1.0.0 Alpha</h4>
+
+        </nav>
+
+    </body>
+
+</html>
--- View/error.php
+++ View/error.php
@@ -1,2 +1,18 @@
-<h1>ERROR 404</h1>
-<h4>Page Not Found.</h4>
+<!DOCTYPE html>
+
+<head>
+
+    <title>PHPShin - Error 404</title>
+
+    <link rel="icon" type="image/icon" href="PHPShin/images/rounded_logo.png">
+
+</head>
+
+    <body>
+        
+        <h1>ERROR 404</h1>
+        <h4>Page Not Found.</h4>
+        
+    </body>
+
+</html>
